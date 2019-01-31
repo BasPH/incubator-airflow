89695a6b6ce2916c88d9f5d10d0feb8def18a9e6
Bas Harenslak <basharenslak@gmail.com>
2019-01-31 22:45:21 +0100

    [AIRFLOW-3797] Improve migration performance by processing DAGs only once

--- airflow/migrations/versions/cc1e65623dc7_add_max_tries_column_to_task_instance.py
+++ airflow/migrations/versions/cc1e65623dc7_add_max_tries_column_to_task_instance.py
@@ -24,6 +24,7 @@ Revises: 127d2bf2dfa7
 Create Date: 2017-06-19 16:53:12.851141
 
 """
+from collections import defaultdict
 
 from alembic import op
 import sqlalchemy as sa
@@ -79,22 +80,35 @@ def upgrade():
             tis = session.query(TaskInstance).filter(
                 TaskInstance.max_tries == -1
             ).limit(BATCH_SIZE).all()
+
+            # Instead of processing the DAG for every TI, first construct a
+            # collection of {dag_id:{task_id:[tis]}} to traverse each DAG only once.
+            dagids_tasks = defaultdict(lambda: defaultdict(list))
             for ti in tis:
-                dag = dagbag.get_dag(ti.dag_id)
-                if not dag or not dag.has_task(ti.task_id):
-                    # task_instance table might not have the up-to-date
-                    # information, i.e dag or task might be modified or
-                    # deleted in dagbag but is reflected in task instance
-                    # table. In this case we do not retry the task that can't
-                    # be parsed.
-                    ti.max_tries = ti.try_number
-                else:
-                    task = dag.get_task(ti.task_id)
-                    if task.retries:
-                        ti.max_tries = task.retries
+                dagids_tasks[ti.dag_id][ti.task_id].append(ti)
+
+            for dag_id, tasks in dagids_tasks.items():
+                dag = dagbag.get_dag(dag_id)
+                for task_id, task_instances in tasks.items():
+                    if not dag or not dag.has_task(task_id):
+                        # task_instance table might not have the up-to-date
+                        # information, i.e DAG or task might be modified or
+                        # deleted in DagBag but is reflected in task_instance
+                        # table. In this case we do not retry the task that can't
+                        # be parsed.
+                        for ti in task_instances:
+                            ti.max_tries = ti.try_number
                     else:
-                        ti.max_tries = ti.try_number
-                session.merge(ti)
+                        task = dag.get_task(task_id)
+                        if task.retries:
+                            for ti in task_instances:
+                                ti.max_tries = task.retries
+                        else:
+                            for ti in task_instances:
+                                ti.max_tries = ti.try_number
+
+                    for ti in task_instances:
+                        session.merge(ti)
 
             session.commit()
         # Commit the current session.
@@ -115,20 +129,31 @@ def downgrade():
             tis = session.query(TaskInstance).filter(
                 TaskInstance.max_tries != -1
             ).limit(BATCH_SIZE).all()
+
+            # Instead of processing the DAG for every TI, first construct a
+            # collection of {dag_id:{task_id:[tis]}} to traverse each DAG only once.
+            dagids_tasks = defaultdict(lambda: defaultdict(list))
             for ti in tis:
-                dag = dagbag.get_dag(ti.dag_id)
-                if not dag or not dag.has_task(ti.task_id):
-                    ti.try_number = 0
-                else:
-                    task = dag.get_task(ti.task_id)
-                    # max_tries - try_number is number of times a task instance
-                    # left to retry by itself. So the current try_number should be
-                    # max number of self retry (task.retries) minus number of
-                    # times left for task instance to try the task.
-                    ti.try_number = max(0, task.retries - (ti.max_tries -
-                                                           ti.try_number))
-                ti.max_tries = -1
-                session.merge(ti)
+                dagids_tasks[ti.dag_id][ti.task_id].append(ti)
+
+            for dag_id, tasks in dagids_tasks.items():
+                dag = dagbag.get_dag(dag_id)
+                for task_id, task_instances in tasks.items():
+                    if not dag or not dag.has_task(task_id):
+                        for ti in task_instances:
+                            ti.try_number = 0
+                    else:
+                        task = dag.get_task(task_id)
+                        # max_tries - try_number is number of times a task instance
+                        # left to retry by itself. So the current try_number should be
+                        # max number of self retry (task.retries) minus number of
+                        # times left for task instance to try the task.
+                        for ti in task_instances:
+                            ti.try_number = max(0, task.retries - (ti.max_tries -
+                                                                   ti.try_number))
+                    for ti in task_instances:
+                        ti.max_tries = -1
+                        session.merge(ti)
             session.commit()
         session.commit()
     op.drop_column('task_instance', 'max_tries')
